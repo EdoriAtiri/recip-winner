c208d6b1f0c55ff080d3631a583dc7e24345367c
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-06 13:20:03 +0100

    FE: add handleSearchSubmit fn to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useState, FormEvent } from 'react'
 import './App.css'
+import { searchRecipes } from './API'
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [recipes, setRecipes] = useState([])
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    try {
+      const { results } = await searchRecipes(searchTerm, 1)
+      setRecipes(results)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return <div>Hello from Recipe App</div>
 }
