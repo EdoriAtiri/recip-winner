5021b8afc34470d1c989cd0c32dad1e1fd6bfeed
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-06 14:46:21 +0100

    FE: add recipesummary type

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,12 +8,20 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+// query recipes
 app.get('/api/recipe/search', async (req, res) => {
   const searchTerm = req.query.searchTerm as string
   const page = parseInt(req.query.page as string)
 
   const results = await RecipeAPI.searchRecipes(searchTerm, page)
   return res.json(results)
+})
+
+// Get recipe summary
+app.get('/api/recipe/:recipeId/summary', async (req, res) => {
+  const recipeId = req.params.recipeId
+  const result = await RecipeAPI.getRecipeSummary(recipeId)
+  res.json(result)
 })
 
 app.listen(5000, () => {
