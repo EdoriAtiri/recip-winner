7971761ef3e36beeebc67fa3c18d5a466ad21daa
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-12-08 16:03:40 +0100

    FE: add isFavorite/onfavouriteBtnClick props to recipecard

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,24 +1,31 @@
 import '../App.css'
-import { AiOutlineHeart } from 'react-icons/ai'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 
 const RecipeCard = ({
   src,
   title,
   click,
+  isFavorite,
+  onFavoriteButtonClick,
 }: {
   src: string
   title: string
   click: React.MouseEventHandler<HTMLButtonElement>
+  isFavorite: boolean
+  onFavoriteButtonClick: React.MouseEventHandler<HTMLButtonElement>
 }) => {
-  const handleFavoriteClick = () => {}
   return (
     <button onClick={click} className='recipeCard'>
       <figure>
         <img src={src} alt='' />
       </figure>
       <div className='recipe-card-title'>
-        <span onClick={handleFavoriteClick}>
-          <AiOutlineHeart size={25} />
+        <span onClick={onFavoriteButtonClick}>
+          {isFavorite ? (
+            <AiFillHeart size={25} color='red' />
+          ) : (
+            <AiOutlineHeart size={25} />
+          )}{' '}
         </span>
         <h3>{title}</h3>
       </div>
